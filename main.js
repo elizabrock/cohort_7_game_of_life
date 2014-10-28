6f882d0fc466b62166b353a103b77007d800a1c0
Eliza Brock <eliza@elizabrocksoftware.com>
2014-10-28 10:42:11 -0500

    Begin implementing `tick`, with stubbed `livingNeighborCount`

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 document.addEventListener('DOMContentLoaded', function(){
-  var matrix = [[0, 0, 0], [1, 1, 1], [0, 0, 0]];
+  var matrix = [[0, 1, 0], [0, 1, 1], [0, 1, 1]];
+  generateGrid(matrix);
 
-  var $table = document.getElementById('grid');
+  function generateGrid(matrix){
+    var $table = document.querySelector('#grid');
+    $table.innerHTML = '';
+    // matrix => [0, 0, 0]
+    //           [1, 1, 1]
+    //           [0, 0, 0]
 
-  // matrix => [0, 0, 0]
-  //           [1, 1, 1]
-  //           [0, 0, 0]
-
-  matrix.forEach(function(row){ // first time, row => [0, 0, 0]
-    // create a tr for the row
-    var $tr = document.createElement('tr');
-    row.forEach(function(cell){ // first time, cell => 0
-      // cell goes into a new td
-      // that td goes into a tr
-      var $td = createTableCell(cell);
-      $tr.appendChild($td);
-      // alternative:
-      // $tr.appendChild( createTableCell(cell) );
+    matrix.forEach(function(row){ // first time, row => [0, 0, 0]
+      // create a tr for the row
+      var $tr = document.createElement('tr');
+      row.forEach(function(cell){ // first time, cell => 0
+        // cell goes into a new td
+        // that td goes into a tr
+        var $td = createTableCell(cell);
+        $td.textContent = cell;
+        $tr.appendChild($td);
+        // alternative:
+        // $tr.appendChild( createTableCell(cell) );
+      });
+      // add that tr to the table
+      $table.appendChild($tr);
     });
-    // add that tr to the table
-    $table.appendChild($tr);
-  });
+  }
 
   function createTableCell(value){
     var $td = document.createElement('td');
@@ -32,4 +36,31 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     return $td;
   }
+
+  function livingNeighborCount(x, y){
+    return 4;
+  }
+
+  function calculateNextState(currentState){
+    var nextState = [];
+    currentState.forEach(function(currentRow, x){
+      var nextRow = [];
+      currentRow.forEach(function(currentCell, y){
+        var nextCellState = livingNeighborCount(x, y);
+        // Rule 1. Less than 2 neighbors = die of loneliness
+        // Rule 2. Things stay the same unless they change (inertia)
+        // Rule 3. More than 3 neighbors = death by overpopulation
+        // Rule 4. Exactly 3 neighbors = birth
+        nextRow.push(nextCellState);
+      });
+      nextState.push(nextRow);
+    });
+    return nextState;
+  }
+
+  document.querySelector("#tick").addEventListener('click', function(){
+    // Tick button has been pressed
+    matrix = calculateNextState(matrix);
+    generateGrid(matrix);
+  });
 });
