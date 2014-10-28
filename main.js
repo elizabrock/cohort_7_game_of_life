0446074920b214a808620ecb77dd6d7eabf3cba4
Eliza Brock <eliza@elizabrocksoftware.com>
2014-10-28 09:54:48 -0500

    Use background colors to denote cell state.

--- main.js
+++ main.js
@@ -12,12 +12,24 @@ document.addEventListener('DOMContentLoaded', function(){
     var $tr = document.createElement('tr');
     row.forEach(function(cell){ // first time, cell => 0
       // cell goes into a new td
-      var $td = document.createElement('td');
-      $td.textContent = cell;
       // that td goes into a tr
+      var $td = createTableCell(cell);
       $tr.appendChild($td);
+      // alternative:
+      // $tr.appendChild( createTableCell(cell) );
     });
     // add that tr to the table
     $table.appendChild($tr);
   });
+
+  function createTableCell(value){
+    var $td = document.createElement('td');
+    // Apply alive or dead class to the td
+    if(value === 1){
+      $td.classList.add('alive');
+    } else {
+      $td.classList.add('dead');
+    }
+    return $td;
+  }
 });
